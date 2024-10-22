9564c2af81e5b21813d91e8f4b78c5b5d03c6285
vanwinkleck <test@test.com>
2024-10-21 22:26:30 -0500

    fixed laptop config (finally). Added readme to help with future machines

--- hosts/despe/configuration.nix
+++ hosts/despe/configuration.nix
@@ -12,9 +12,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "despe"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
