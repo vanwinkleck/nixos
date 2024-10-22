1a2ecdeeb4f8c3589127f3ce261599eb4ed3ba8c
vanwinkleck <vaniwnkleck@test.com>
2024-10-21 22:01:43 -0500

    trying to fix laptop config

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
      };
 
      nix-colors.url = "github:misterio77/nix-colors";
-
      #hyprland.url = "github:hyprwm/Hyprland";
      #hyprland-plugins = {
      #	url = "github:hyprwm/hyprland-plugins";
@@ -25,6 +24,14 @@
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
+    };
+
+    nixosConfigurations.despe = nixpkgs.lib.nixosSystem {
+     specialArgs = {inherit inputs;};
+     modules = [
+      ./hosts/despe/configuration.nix
+      inputs.home-manager.nixosModules.default
+     ];
     };
   };
 }
--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -70,7 +70,7 @@
   home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    hello
+    #hello
     alacritty
     librewolf
     #pkgs.heroic
