d5e18df1a49955bdc5331e7db1eeebc337e25cd2
vanwinkleck <vanwinkleck@protonmail.com>
2024-11-11 19:01:58 -0600

    minor fix, flake update

--- modules/home-manager/features/alacritty.nix
+++ modules/home-manager/features/alacritty.nix
@@ -23,7 +23,7 @@
 		#};
 
 	};
- 	colors = with config.colorScheme.colors; {
+ 	colors = with config.colorScheme.palette; {
 	 bright = { 
 		black = "0x${base00}";
         	blue = "0x${base0D}";
--- modules/home-manager/features/nvim/plugin/cmp.lua
+++ modules/home-manager/features/nvim/plugin/cmp.lua
@@ -43,6 +43,5 @@ cmp.setup {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
 		{ name = 'clangd_extensions' },
-		{ name = 'zig-vim' },
     },
 }
