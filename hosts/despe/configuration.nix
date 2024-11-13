cf5051e2ba34f542a4f8a5c0af505bda8d3f19cc
vanwinkleck <vanwinkleck@protonmail.com>
2024-11-13 09:15:52 -0600

    beginning to seperate hyprland configuration

--- hosts/despe/configuration.nix
+++ hosts/despe/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
+      #./../../modules/home-manager/features/hyprland.nix
     ];
 
   # Bootloader.
