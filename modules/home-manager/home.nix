34d42828836a0bd7a495af5444956b4a768a17d3
vanwinkleck <vanwinkleck@protonmail.com>
2024-11-16 21:54:50 -0600

    cleaned up home

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -158,48 +158,6 @@
 
   };
 
-  #programs.zsh = {
-  #enable = true;
-  #enableCompletion = true;
-  #autosuggestion.enable = true;
-  #syntaxHighlighting.enable = true;
-
-  #shellAliases = {
-    # Misc
-  #  ll = "ls -l";
-  # ls = "ls -a --color=auto";
-
-    # Nix commands 
-  # fupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#friday";
-  # dupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#despe";
-  # uflake = "sudo nix flake update";
-  # ncg = "sudo nix-collect-garbage";
-  # opt = "sudo nix store optimise";
-
-    # Navigation
-  # edits = "cd /mnt/makima/Edits/";
-  # dot = "cd /home/rip/.config/";
-  # config = "cd /home/rip/nixos/";
-
-    # Git commands
-  # gc = "git commit -m";
-  # gps = "git push";
-  # gpl = "git pull";
-  # };
-
-  # history = {
-  # size = 10000;
-  # path = "${config.xdg.dataHome}/zsh/history";
-  #};
-
-  #oh-my-zsh = {
-  # enable = true;
-  # theme = "steeef";
-  # plugins = ["git" "thefuck" "ufw" "starship" "tldr"];
-
-  #};  
-  #};
-  
  # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
