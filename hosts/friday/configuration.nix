03f2f67066be9de1e4ebc1243c00f9c821c24fd7
vanwinkleck <vanwinkleck@protonmail.com>
2024-11-25 13:11:41 -0600

    modularized disks

--- hosts/friday/configuration.nix
+++ hosts/friday/configuration.nix
@@ -12,6 +12,7 @@
       ../../modules/system/fonts.nix
       ../../modules/system/virtualisation.nix
       ../../modules/system/steam.nix
+      ../../modules/system/disks.nix
     ];
 
   # Bootloader.
@@ -27,29 +28,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  
-
-  fileSystems."/mnt/makima" =
-    { device = "/dev/disk/by-uuid/E6FB-AEB8";
-      fsType = "exfat";
-      options = ["nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
-    };
-
-  fileSystems."/mnt/reze" =
-    { device = "/dev/disk/by-uuid/1b5857f8-5912-4229-9939-c265d0bc332c";
-      fsType = "ext4";
-      options = ["nofail" "rw" "user" "exec"];#["nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
-    };
-
-
-  
-  fileSystems."/mnt/asa" = 
-  {
-   device= "/dev/disk/by-uuid/4cd22232-9f91-4f7c-a878-3a4c386f39e5";
-   fsType = "ext4";
-   options = [ "nofail" "rw" "user" "exec" ];#[ "nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
-  };
-
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
---
+++ modules/system/disks.nix
@@ -0,0 +1,22 @@
+{ ... }: {
+
+   fileSystems."/mnt/makima" =
+    { device = "/dev/disk/by-uuid/E6FB-AEB8";
+      fsType = "exfat";
+      options = ["nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+    };
+
+  fileSystems."/mnt/reze" =
+    { 
+      device = "/dev/disk/by-uuid/1b5857f8-5912-4229-9939-c265d0bc332c";
+      fsType = "ext4";
+      options = ["nofail" "rw" "user" "exec"];#["nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+    };
+
+fileSystems."/mnt/asa" = 
+  {
+   device= "/dev/disk/by-uuid/4cd22232-9f91-4f7c-a878-3a4c386f39e5";
+   fsType = "ext4";
+   options = [ "nofail" "rw" "user" "exec" ];#[ "nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+  };
+}
