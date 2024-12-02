03d10a5a136e2d9a92ff70624020ec74902e13f8
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-02 10:09:23 -0600

    renamed homePackages to make navigation easier

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -3,7 +3,7 @@
 {
  imports = [
  	inputs.nix-colors.homeManagerModules.default
-    ./homePackages.nix
+    ./packagesHome.nix
     ./features/alacritty.nix
     ./features/mpv.nix
     ./features/discord.nix
---
+++ modules/home-manager/packagesHome.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+
+  home.packages = with pkgs; [
+    alacritty
+    librewolf
+    qbittorrent
+    nicotine-plus
+    htop
+
+    oh-my-zsh
+    thefuck
+    starship
+    tldr
+
+    anki
+
+    komikku
+
+  ];
+}
