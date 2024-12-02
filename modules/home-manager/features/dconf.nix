bfff704c2e80f9461e6feec0a4339334084686ca
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-02 16:02:56 -0600

    gnome update

--- modules/home-manager/features/dconf.nix
+++ modules/home-manager/features/dconf.nix
@@ -10,6 +10,14 @@
       enable-hot-corners = false;
     };
 
+    "org/gnome/shell" = {
+      disable-user-extensions = false; 
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        user-themes.extensionUuid
+        removable-drive-menu.extensionUuid
+      ];
+    };
+
   };
 
 }
