fb1ebec50544b55338b77420087e2966b96365a9
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-01 20:27:44 -0600

    minor fix

--- modules/system/disks.nix
+++ modules/system/disks.nix
@@ -13,10 +13,10 @@
       options = ["nofail" "rw" "user" "exec"];#["nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
     };
 
-fileSystems."/mnt/asa" = 
-  {
-   device= "/dev/disk/by-uuid/4cd22232-9f91-4f7c-a878-3a4c386f39e5";
-   fsType = "ext4";
-   options = [ "nofail" "rw" "user" "exec" ];#[ "nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
-  };
+  fileSystems."/mnt/asa" = 
+    {
+     device= "/dev/disk/by-uuid/4cd22232-9f91-4f7c-a878-3a4c386f39e5";
+     fsType = "ext4";
+     options = [ "nofail" "rw" "user" "exec" ];#[ "nofail" "uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+    };
 }
