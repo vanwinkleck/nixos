6b918a9ad48890bfa51e88cf84bdba3224c67405
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-01 19:57:02 -0600

    cleanup

--- modules/system/emulation.nix
+++ modules/system/emulation.nix
@@ -1,13 +1,4 @@
 { pkgs, ... }:
-  #environment.systemPackages = with pkgs; [
-  #retroarch.withCores {
-  #    (cores = with cores; [
-  #     mgba
-  #     melonds
-  #     dolphin
-  #     citra
-  #   ]);
-  # }];
 let
   retroarchWithCores = (pkgs.retroarch.withCores (cores: with cores; [
     mgba
