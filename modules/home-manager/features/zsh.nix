f89d9414b83aefd75bf8095744d63aa6451dce9a
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-18 11:33:50 -0600

    flake update, added ffmpeg shell aliases. Right now just sed-ing output for easy copy paste. Eventually want to make script that accepts variables

--- modules/home-manager/features/zsh.nix
+++ modules/home-manager/features/zsh.nix
@@ -7,40 +7,42 @@ programs.zsh = {
   syntaxHighlighting.enable = true;
 
    shellAliases = {
-    # Misc
-    ll = "ls -l";
-    ls = "ls -a --color=auto";
+      # Misc
+      ll = "ls -l";
+      ls = "ls -a --color=auto";
 
-    # Nix commands 
-    fupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#friday";
-    dupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#despe";
-    uflake = "sudo nix flake update";
-    ncg = "sudo nix-collect-garbage";
-    opt = "sudo nix store optimise";
+      # Nix commands 
+      fupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#friday";
+      dupdate = "sudo nixos-rebuild switch --flake /home/rip/nixos/#despe";
+      uflake = "sudo nix flake update";
+      ncg = "sudo nix-collect-garbage";
+      opt = "sudo nix store optimise";
 
-    # Navigation
-    edits = "cd /mnt/makima/Edits/";
-    dot = "cd /home/rip/.config/";
-    config = "cd /home/rip/nixos/";
 
-    # FFMPEG
-      #mp4 = "ffmpeg -i $input.mp4 -c:v libx264 -crf $quality -preset slow -vf format=yuv420p -c:a aac -movflags +faststart $output.mp4";
+      # Navigation
+      edits = "cd /mnt/makima/Edits/";
+      dot = "cd /home/rip/.config/";
+      config = "cd /home/rip/nixos/";
 
-    # Git commands
-    gc = "git commit -m";
-    gps = "git push";
-    gpl = "git pull";
+      # FFMPEG
+      trim = "sed -n -e 6p /home/rip/Documents/commands";
+      conv = "sed -n -e 22p /home/rip/Documents/commands";
+
+      # Git commands
+      gc = "git commit -m";
+      gps = "git push";
+      gpl = "git pull";
     };
 
-    history = {
-    size = 10000;
-    path = "${config.xdg.dataHome}/zsh/history";
+      history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
    };
 
-   oh-my-zsh = {
-    enable = true;
-    theme = "steeef";
-    plugins = ["git" "thefuck" "ufw" "starship" "tldr"];
+    oh-my-zsh = {
+      enable = true;
+      theme = "steeef";
+      plugins = ["git" "thefuck" "ufw" "starship" "tldr"];
 
     };  
   };
