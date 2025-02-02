e5755667dace80b3d8911e07fe87b45d16711d12
vanwinkleck <vanwinkleck@protonmail.com>
2025-02-01 22:11:48 -0600

    gpu acceleration for ollama should be working now for real this time

--- modules/system/ollama.nix
+++ modules/system/ollama.nix
@@ -9,10 +9,10 @@
     enable = true;
     loadModels = ["mistral" "deepseek-r1:8b"];
     acceleration = "rocm";
-    #environmentVariables = {
-    #  HCC_AMDGPU_TARGET = "gfx1030";
-    #};
-    #rocmOverrideGfx = "10.3.0";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1030";
+    };
+    rocmOverrideGfx = "10.3.0";
   };
 
   # might not need this, going to try default web ui first
