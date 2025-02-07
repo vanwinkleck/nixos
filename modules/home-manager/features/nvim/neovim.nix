f85f73ceb072d2c313f74852058a5f4f0e429208
vanwinkleck <vanwinkleck@protonmail.com>
2025-02-06 19:26:23 -0600

    flake update, neovim update

--- modules/home-manager/features/nvim/neovim.nix
+++ modules/home-manager/features/nvim/neovim.nix
@@ -2,97 +2,113 @@
 {
   
   programs.neovim = 
-	let 
+    let
 		toLua = str: "lua << EOF\n${str}\nEOF\n";
 		toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-	in
-	{
-	enable = true;
+    in
+	  {
+	  enable = true;
 
-	extraPackages = with pkgs; [
-		lua-language-server
+	  extraPackages = with pkgs; [
+		  lua-language-server
 		#rnix-lsp
 
-		xclip
-		wl-clipboard
+		  xclip
+		  wl-clipboard
 
-		luajitPackages.lua-lsp
-	];
-
-	viAlias = true;
-	vimAlias = true;
-	vimdiffAlias = true;
-
-	extraLuaConfig = ''
-
-	${builtins.readFile ./options.lua}
-
-	'';
-
-	plugins = with pkgs.vimPlugins; [
-	 {
-        plugin = nvim-lspconfig;
-        config = toLuaFile ./plugin/lsp.lua;
-      }
-
-      {
-        plugin = comment-nvim;
-        config = toLua "require(\"Comment\").setup()";
-      }
-
-      {
-        plugin = kanagawa-nvim;
-        config = "colorscheme kanagawa";
-      }
-
-      neodev-nvim
-
-      mason-nvim
-      mason-lspconfig-nvim
-
-      nvim-cmp 
-      {
-        plugin = nvim-cmp;
-        config = toLuaFile ./plugin/cmp.lua;
-      }
-
-      {
-        plugin = telescope-nvim;
-        config = toLuaFile ./plugin/telescope.lua;
-      }
-
-      telescope-fzf-native-nvim
-
-      cmp_luasnip
-      cmp-nvim-lsp
+		  luajitPackages.lua-lsp
+      ];
 
 
-      luasnip
-      friendly-snippets
+      viAlias = true;
+	  vimAlias = true;
+	  vimdiffAlias = true;
 
+	  extraLuaConfig = ''
 
-      lualine-nvim
-      nvim-web-devicons
+	  ${builtins.readFile ./options.lua}
 
-      vim-clang-format
-      clangd_extensions-nvim
+	  '';
 
-      zig-vim 
-
+      plugins = with pkgs.vimPlugins; [
         {
-          plugin = (nvim-treesitter.withPlugins (p: [
-          p.tree-sitter-nix
-          p.tree-sitter-vim
-          p.tree-sitter-bash
-          p.tree-sitter-lua
-          p.tree-sitter-python
-          p.tree-sitter-json
-          p.tree-sitter-zig
-          ]));
-         config = toLuaFile ./plugin/treesitter.lua;
+          plugin = nvim-lspconfig;
+          config = toLuaFile ./plugin/lsp.lua;
         }
 
-      vim-nix
+        {
+          plugin = comment-nvim;
+          config = toLua "require(\"Comment\").setup()";
+        }
+
+        {
+          plugin = kanagawa-nvim;
+          config = "colorscheme kanagawa";
+        }
+
+
+        neodev-nvim
+
+        mason-nvim
+        mason-lspconfig-nvim
+
+        nvim-cmp 
+
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./plugin/cmp.lua;
+        }
+
+        {
+          plugin = telescope-nvim;
+          config = toLuaFile ./plugin/telescope.lua;
+        }
+
+        telescope-fzf-native-nvim
+
+        cmp_luasnip
+        cmp-nvim-lsp
+
+
+        luasnip
+        friendly-snippets
+
+
+        lualine-nvim
+        nvim-web-devicons
+
+        vim-clang-format
+        
+        clangd_extensions-nvim
+
+        zig-vim
+
+        phpactor
+
+        coc-tsserver
+
+
+        {
+
+          plugin = (nvim-treesitter.withPlugins (p: [
+            p.tree-sitter-nix
+            p.tree-sitter-vim
+            p.tree-sitter-bash
+            p.tree-sitter-lua
+            p.tree-sitter-cpp
+            p.tree-sitter-cmake
+            p.tree-sitter-javascript
+            p.tree-sitter-dockerfile
+            p.tree-sitter-php
+            p.tree-sitter-python
+            p.tree-sitter-json
+            p.tree-sitter-zig
+
+          ]));
+          config = toLuaFile ./plugin/treesitter.lua;
+        }
+
+        vim-nix
 
 	];
 
