220f68f2d55ba2c3509a5381855ba975dd497345
vanwinkleck <vanwinkleck@protonmail.com>
2025-03-28 13:56:22 -0500

    maybe fixed

--- modules/home-manager/features/discord.nix
+++ modules/home-manager/features/discord.nix
@@ -15,19 +15,6 @@
           desktopName = "Discord";
           exec = "vesktop --enable-features=VaapiIgnoreDriverChecks,VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,UseMultiPlaneFormatForHardwareVideo";
           icon = "discord";
-           startupWMClass = "VencordDesktop";
-          genericName = "Internet Messenger";
-     keywords = [
-             "discord"
-             "vencord"
-             "electron"
-            "chat"
-          ];
-           categories = [
-            "Network"
-            "InstantMessaging"
-            "Chat"
-           ];
           })
        ];
       });
